f52b4aafdf2ddbfbd4ba1e0cd0f6526c0bdaac8c
Fabian Gans <fabiangans@gmx.de>
2014-03-07 14:20:17 +0100

    deleted old files

--- gmt_init.jl
+++
@@ -1,4 +0,0 @@
-# Julia wrapper for header: /opt/local/lib/gmt4/include/gmt_init.h
-# Automatically generated using Clang.jl wrap_c, version 0.0.0
-
-
--- gmt_version.jl
+++
@@ -1,4 +0,0 @@
-# Julia wrapper for header: /opt/local/lib/gmt4/include/gmt_version.h
-# Automatically generated using Clang.jl wrap_c, version 0.0.0
-
-
